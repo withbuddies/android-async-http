ba227fc87906afa1883e6c42a8e4fae78ebe22ac
John Lombardo <john@scopely.com>
2012-10-05 00:29:01 -0700

    Add onXXX() to interface

--- src/com/loopj/http/AsyncHttpResponseHandler.java
+++ src/com/loopj/http/AsyncHttpResponseHandler.java
@@ -3,6 +3,12 @@ package com.loopj.http;
 import org.apache.http.HttpResponse;
 
 public interface AsyncHttpResponseHandler {
+	public void onStart();
+	public void onFinish();
+	public void onSuccess(String content);
+	public void onSuccess(int statusCode, String content);
+	public void onFailure(Throwable error);
+	public void onFailure(Throwable error, String content);
 	public void sendStartMessage();
 	public void sendFinishMessage();
 	public void sendFailureMessage(Throwable e, String responseBody);
